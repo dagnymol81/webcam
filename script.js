ceef0dc148847fd65fa2100aeff7e6e278dd4e2f
Dagny Mol <dagnymol@gmail.com>
2023-02-04 12:53:39 -0500

    fixed camera select, added play and stop buttons

--- script.js
+++ script.js
@@ -1,44 +1,60 @@
-const video = document.querySelector('video');
-const constraints = {
-  audio: true,
-  video: true
-};
-
 const cameraOptions = document.querySelector('.custom-select');
+const play = document.querySelector('.play')
+const stop = document.querySelector('.stop')
 
-navigator.mediaDevices.getUserMedia(constraints)
-  .then((stream) => {
-    const videoTracks = stream.getVideoTracks();
-    console.log('Got stream with constraints:', constraints);
-    console.log(`Using video device: ${videoTracks[0].label}`);
-    stream.onremovetrack = () => {
-      console.log('Stream ended');
-    };
-    video.srcObject = stream;
-  })
-  .catch((error) => {
-    if (error.name === 'ConstraintNotSatisfiedError') {
-      console.error(
-        `The resolution ${constraints.video.width.exact}x${constraints.video.height.exact} px is not supported by your device.`
-      );
-    } else if (error.name === 'PermissionDeniedError') {
-      console.error(
-        'Permissions have not been granted to use your camera and ' +
-          'microphone, you need to allow the page access to your devices in ' +
-          'order for the demo to work.'
-      );
-    } else {
-      console.error(`getUserMedia error: ${error.name}`, error);
+const getCameraSelection = async () => {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const videoDevices = devices.filter(device => device.kind === 'videoinput');
+  const options = videoDevices.map(videoDevice => {
+    return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
+  });
+  cameraOptions.innerHTML = options.join('');
+}
+
+window.onload = getCameraSelection();
+
+
+const startStream = async () => {
+  navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: {
+      deviceId: {
+        exact: cameraOptions.value
+      }
     }
+  })
+  .then((mediaStream) => {
+    const video = document.querySelector('video');
+    video.srcObject = mediaStream;
+    video.onloadedmetadata = () => {
+      video.play();
+    };
+  })
+  .catch((err) => {
+    console.error(`${err.name}: ${err.message}`);
+  });
+}
+
+const stopPlayback = () => {
+  const videoElem = document.querySelector('video')
+  const stream = videoElem.srcObject;
+  const tracks = stream.getTracks();
+
+  tracks.forEach((track) => {
+    track.stop();
   });
 
-  const getCameraSelection = async () => {
-    const devices = await navigator.mediaDevices.enumerateDevices();
-    const videoDevices = devices.filter(device => device.kind === 'videoinput');
-    const options = videoDevices.map(videoDevice => {
-      return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
-    });
-    cameraOptions.innerHTML = options.join('');
-  };
+  videoElem.srcObject = null;
+}
 
- window.onload = getCameraSelection();
+cameraOptions.addEventListener("click", (event) => {
+  startStream()
+})
+
+play.addEventListener("click", (event) => {
+  startStream()
+})
+
+stop.addEventListener("click", (event) => {
+  stopPlayback()
+})
